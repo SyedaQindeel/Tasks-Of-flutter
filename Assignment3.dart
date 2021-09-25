ef933b8e464b24b12e942a01ac4d6b599bd6e7d0
Syeda Qindeel Zehra <syedaqindeel01@gmail.com>
2021-09-25 23:01:19 +0500

    Assignment 3

---
+++ Assignment3.dart
@@ -0,0 +1,120 @@
+void main()
+{
+                           // Answer # 1
+//   print("ANSWER NO. 1");
+//   List<String> ballList = ["bilal","ali","owais","bilal","owais"];
+//   List<String> Updated = ballList.toSet().toList();
+//   print(Updated);
+
+// print("=======================================================================================================================================\n\t");
+                               // Answer # 2
+//     print("ANSWER NO. 2");
+//   var list1 = [1,2,3,4,5,6,7];
+//   var list2 = [3,5,6,7,9,10];
+//   print(list1.where((e) => !list2.contains(e)).toList());
+
+// print("=======================================================================================================================================\n\t");
+                               // Answer # 3
+//    print("ANSWER NO. 3");
+//    List<int> a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+//    List<int> b = [];
+//    for (int i = 0 ; i<= a.length-1 ; i++)
+//   {
+//     if(a[i]%2 == 0)
+//     {
+//       b.add(a[i]);
+//     }
+//   }
+//   print("Original List: $a");
+//   print("Only Even numbers from list : $b");
+  
+// print("=======================================================================================================================================\n\t");
+                               // Answer # 4
+//     print("ANSWER NO. 4");
+//     int number = 29 , check=0 , i;
+//     for(i=2; i<number; i++)
+//     {
+//         if(number%i == 0)
+//         {
+//             check++;
+//             break;
+//         }
+//     }
+//     if(check==0)
+//       print("${number} : It is a Prime Number");
+    
+//   else
+//       print("${number} : It is not a Prime Number");
+  
+// print("=======================================================================================================================================\n\t");
+                               // Answer # 5
+//      print("ANSWER NO. 5");
+//      int table = 7;
+//      for (int num=1 ; num<=15 ; num++)
+//      {
+//        print("$table X $num = ${table*num}");
+//      }
+  
+// print("=======================================================================================================================================\n\t");
+                               // Answer # 6
+//      print("ANSWER NO. 6");
+//      List<String> fruits = ["apple","banana","mango","orange","strawberry"];
+//      print("\n\t\tFruits List\n");
+//      for(int i=0 ; i<= fruits.length-1 ; i++)
+//      {
+//        print(fruits[i]);
+//      }
+  
+// print("=======================================================================================================================================\n\t");
+                               // Answer # 7
+//       print("ANSWER NO. 7");
+//       List<int> num = [];
+//       for (int i=1 ; i<= 100 ; i++)
+//       {
+//         if(i%5 == 0)
+//         {
+//           num.add(i);
+//         }
+//       }
+      
+//       print("Multiples of 5 ranging 1 to 100: $num");
+  
+// print("=======================================================================================================================================\n\t");
+                               // Answer # 8
+//        print("ANSWER NO. 8");
+//        num temp = 76 , C=0 , F=0;
+//        C = ((temp*(9/5))+32);
+//        F = (temp - 32)*(5/9);
+//        print("NNoC is NNoF : $C");
+//        print("NNoF is NNoC : $F");
+  
+// print("=======================================================================================================================================\n\t");
+                               // Answer # 9
+//        print("ANSWER NO. 9");
+//        num fno=6 , sno=4 , ano=0;
+//        String op = "*";
+//        if(op == "+")
+//        {ano = fno+sno;}
+//        else if(op == "-")
+//        { ano = fno+sno;}
+//        else if(op == "*")
+//        { ano = fno*sno;}
+//         else if(op == "/")
+//        { ano = fno/sno;}
+//        else
+//        { ano = fno%sno;}
+//        print("$fno $op $sno = $ano");
+  
+// print("=======================================================================================================================================\n\t");
+                               // Answer # 10
+//        print("ANSWER NO. 10");
+//        String i= "A";
+//        if(i=="a"||i=="A"||i=="e"||i=="E"||i=="i"||i=="I"||i=="o"||i=="O"||i=="u"||i=="U")
+//        {
+//          print("$i is vowel");
+//        }
+//        else
+//        {
+//          print("$i is consonant");
+//        }
+}
